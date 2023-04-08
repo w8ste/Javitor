aec87e1210f13b71fcb479d5970f105318e7be5f
Hannes Albert <hannes10042004@icloud.com>
2023-04-08 20:11:40 +0200

    Added a button

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,8 @@ public class Main {
     }
 
     public static void start() {
-        MyFrame mainFrame = new MyFrame();
-        mainFrame.makeButton();
-        mainFrame.makeTextArea();
+        new MyFrame();
+
     }
 
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,33 +2,40 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.JButton;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.EventListener;
 public class MyFrame extends JFrame {
+
+    private JButton jButton;
+    private JTextArea textArea;
     public MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
-
-
-
-
-
+        makeButton();
+        makeTextArea();
 
     }
 
     public void makeButton() {
-        JButton b = new JButton();
-        b.setBounds(200, 0, 75, 35);
-        this.add(b);
+        jButton = new JButton();
+        jButton.setBounds(200, 3, 75, 35);
+        jButton.addActionListener(e -> {
+            //this is a placeholder
+            if(e.getSource() == jButton) System.out.println(textArea.getText());
+        });
+
+        this.add(jButton);
     }
 
     public void makeTextArea() {
-        JTextArea area = new JTextArea();
-        area.setVisible(true);
-        area.setLayout(null);
-        area.setBounds(0, 40, 500, 500);
-        this.add(area);
+        textArea = new JTextArea();
+        textArea.setVisible(true);
+        textArea.setLayout(null);
+        textArea.setBounds(0, 41, 500, 500);
+        this.add(textArea);
 
     }
 
